d639a5e0a4b1413e6e994a0f45de75762b216e4a
Alexander Goussas <goussasalexander@gmail.com>
2025-06-29 17:33:16 -0500

    feat: fetch student data for insertion in postgres

--- src/Exporter/Postgres.hs
+++ src/Exporter/Postgres.hs
@@ -2,6 +2,9 @@ module Exporter.Postgres (exporter) where
 
 import           Control.Exception
 import           Control.Monad
+import           Control.Monad.IO.Class     (liftIO)
+import qualified Control.Monad.IO.Class
+import           Data.Aeson                 (FromJSON)
 import qualified Data.ByteString.Char8      as B8
 import qualified Data.List                  as L
 import           Data.String                (fromString)
@@ -9,6 +12,8 @@ import qualified Data.Text                  as T
 import qualified Data.Text.IO               as TIO
 import qualified Database.PostgreSQL.Simple as Pg
 import           Exporter
+import           GHC.Generics               (Generic)
+import           Network.HTTP.Req
 import           System.Environment
 import           Types
 
@@ -29,15 +34,19 @@ insertRows :: Pg.Connection -> Header -> Rows -> IO  ()
 insertRows conn header rows = do
   tableName <- createTable
 
-  let values :: Row -> Row = \row -> map (\c -> "'" <> (if T.null c then "X" else c) <> "'") row
-  let insertValues = T.intercalate "," (map (\r -> "(" <> (T.intercalate "," . values $ r) <> ")") rows)
+  rowsWithNames <- forM rows $ \row -> do
+      sd <- fetchStudentNames row
+      return (nombres sd : apellidos sd : row)
+
+  let values :: Row -> Row = \row -> map (\c -> "'" <> (if T.null c then "0" else c) <> "'") row
+  let insertValues = T.intercalate "," (map (\r -> "(" <> (T.intercalate "," . values $ r) <> ")") rowsWithNames)
   let q = fromString $ "insert into " <> tableName <> " values " <> fromString (T.unpack insertValues)
 
   void $ Pg.execute_ conn q
   where
     createTable =  do
       let tableName = "Students_Grades_" <> show (length rows)
-      let cols = zipWith (\ x s -> mconcat ["\"", s <> "_" <> T.pack (show x), "\" text"]) [1..] header
+      let cols = "\"Nombres\" text" : "\"Apellidos\" text" : zipWith (\ x s -> mconcat ["\"", s <> "_" <> T.pack (show x), "\" text"]) [1..] header
       let q =  fromString $ mconcat [ "create table if not exists ", tableName, " (" , L.intercalate "," (map T.unpack cols) , ")" ]
       void $ Pg.execute_ conn q
       return tableName
@@ -45,77 +54,29 @@ insertRows conn header rows = do
 disconnect :: Pg.Connection -> IO ()
 disconnect = Pg.close
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+data StudentData = MkStudentData
+  { nombres   :: !T.Text
+  , apellidos :: !T.Text
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON)
+
+
+fetchStudentNames :: Control.Monad.IO.Class.MonadIO m => Row -> m StudentData
+fetchStudentNames row = do
+  let studentEmail =
+        if "@espol.edu.ec" `T.isSuffixOf` head row
+        then head row
+        else head row <> "@espol.edu.ec"
+  liftIO $ TIO.putStrLn $ "Fetching student names for: " <> studentEmail
+  runReq defaultHttpConfig $ do
+    r <- req
+      GET
+      (https "wsarchivos.espol.edu.ec" /: "api" /: "consultas" /: "persona" /: studentEmail)
+      NoReqBody
+      jsonResponse
+      mempty
+    return $ head (responseBody r :: [StudentData])
 
 
 
